521b72d6f0551e54e1fc4bd79adcdaf58a740c69
Micah Halter <micah@mehalter.com>
2019-04-08 18:55:48 -0400

    Updated server

--- server.py
+++ server.py
@@ -12,13 +12,11 @@ Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 
 class CustomHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
-        if (self.path=="/refresh"):
-            subprocess.check_call(['%s/updateGit.sh' % FOLDER, FOLDER])
-            self.send_response(301)
-            self.send_header('Location','/')
-            self.end_headers()
-            return
-        Handler.do_GET(self)
+        subprocess.check_call(['%s/updateGit.sh' % FOLDER, FOLDER])
+        self.send_response(301)
+        self.send_header('Location','/')
+        self.end_headers()
+        return
 
 httpd = SocketServer.TCPServer(("", PORT), CustomHandler)
 
