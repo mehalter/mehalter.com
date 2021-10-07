46adf600575b975f391cf8c5d8a904911231a0aa
Micah Halter <micah@mehalter.com>
2021-10-07 14:29:00 -0400

    Add Drone CI/CD Pipeline (#1)
    Signed-off-by: Micah Halter <micah@balena.io>
    Reviewed-on: https://git.mehalter.com/mehalter/mehalter.com/pulls/1
    Co-authored-by: Micah Halter <micah@mehalter.com>
    Co-committed-by: Micah Halter <micah@mehalter.com>

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pandoc content.md --metadata pagetitle="..." --template=templates/template.tex -o resume.pdf
-pandoc content.md --metadata pagetitle="..." --template=templates/template.html -o index.html
-pandoc content.md --metadata pagetitle="..." --template=templates/man-template.html -o micah.1.html
-pandoc content.md --metadata pagetitle="..." --template=templates/man-template.man -o micah.1
--- updateGit.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-git -C "$1" pull
